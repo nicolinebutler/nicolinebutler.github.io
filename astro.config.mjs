12ed26a4c1ac53346995482dc0f896e68ff5254f
Nate Butler <iamnbutler@gmail.com>
2025-02-11 21:48:47 -0500

    Add R and Python highlighting
    Co-authored-by: nicolinebutler <butlern@hawaii.edu>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,17 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://nicolinebutler.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      langs: [
+        "javascript",
+        "typescript",
+        "css",
+        "html",
+        "markdown",
+        "r",
+        "python",
+      ],
+    },
+  },
 });
